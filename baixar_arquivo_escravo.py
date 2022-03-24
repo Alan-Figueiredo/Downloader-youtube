fb2542f425848de2ef526c50cfdd05b99d5fa0b2
PyAlan <98132046+PyAlan@users.noreply.github.com>
2022-03-24 08:49:59 -0300

    Update baixar_arquivo_escravo.py

--- baixar_arquivo_escravo.py
+++ baixar_arquivo_escravo.py
@@ -5,8 +5,7 @@ from pathlib import *
 home = Path.home() # Chamar a pasta User do sistema 
 desktop = home / 'desktop' # Abrir a pasta desktop
 
-#yt = YouTube(input('Coloque o link aqui: '))
-yt = YouTube('https://www.youtube.com/watch?v=8qcdZfUd4qA&ab_channel=GaleradoghettoOficial')
+yt = YouTube(input('Coloque o link aqui: '))
 yt.streams.filter(only_audio = True)
 
 stream = yt.streams.get_by_itag(251)
